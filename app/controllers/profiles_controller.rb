02e2887929a12008011c8b9d9aeb90dcdaa5d53f
Aurelien Dobbels <adobbels@gmail.com>
2018-11-13 14:36:47 +0100

    new + show + update profile views done

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,48 @@
+class ProfilesController < ApplicationController
+
+  def index
+    @profiles = Profile.all
+  end
+
+  def show
+    @profile = current_user.profile
+  end
+
+  def new
+    @profile = Profile.new
+  end
+
+  def create
+    @profile = Profile.new(profile_params)
+    @profile.user = current_user
+    if @profile.save
+      flash[:notice] = 'Profile was successfully created.'
+      redirect_to profile_path(@profile)
+    else
+      flash[:notice] = 'Your profile has not been created, please update your profile later.'
+      redirect_to root_path
+    end
+  end
+
+  def edit
+    @profile = current_user.profile
+  end
+
+  def update
+    @profile = current_user.profile
+    if @profile.update(profile_params)
+      flash[:notice] = 'Profile was successfully updated.'
+      redirect_to profile_path(@profile)
+    else
+      flash[:notice] = 'Error.  Something went wrong.'
+      render :edit
+    end
+  end
+
+  private
+
+  def profile_params
+  params.require(:profile).permit(:first_name, :last_name)
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_13_091733) do
+ActiveRecord::Schema.define(version: 2018_11_13_105101) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2018_11_13_091733) do
     t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +36,5 @@ ActiveRecord::Schema.define(version: 2018_11_13_091733) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "profiles", "users"
 end
