3d38f77883a37ca499b9b58d75da4a156ab22eb2
Aurelien Dobbels <adobbels@gmail.com>
2018-11-28 15:04:00 +0100

    prod emails working

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -4,8 +4,6 @@ ActionMailer::Base.smtp_settings = {
   domain: 'gmail.com',
   user_name: ENV['GMAIL_ADDRESS'],
   password: ENV['GMAIL_APP_PASSWORD'],
-  # user_name: "adobbels@gmail.com",
-  # password: "klzolmrnekczpwqw",
   authentication: :login,
   enable_starttls_auto: true
 }
